34c79123812bc251dab8c694f29a9d7234f28774
stanislawlewandowski <52546141+stanislawlewandowski@users.noreply.github.com>
2019-09-09 13:30:40 +0200

    add up to date exercise

---
+++ moudl math 2.py
@@ -0,0 +1,15 @@
+small_pizza_r = 22
+big_pizza_r = 27
+family_pizza_r = 38
+
+small_pizza_price = 11.50
+big_pizza_price = 15.60
+family_pizza_price = 22.00
+
+small_pizza_square_meters = small_pizza_r ** 2
+big_pizza_square_meters = big_pizza_r ** 2
+family_pizza_square_meters = family_pizza_r ** 2
+
+print(small_pizza_square_meters)
+print(big_pizza_square_meters)
+print(family_pizza_square_meters)
